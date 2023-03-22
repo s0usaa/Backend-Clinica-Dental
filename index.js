53c52b63a8da335ad7993360bedcb8f9058b3529
David Soto Sousa <sousavlc86@gmail.com>
2023-03-22 12:47:59 +0100

    Añadir /welcome en el index

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 4000;
 const cors = require('cors')
 app.use(express.json());
 
+app.get('/welcome', (req, res) => {
+    return res.send("Bienvenido a mi app")
+})
+
 const router = require("./router")
 app.use(router)
 
